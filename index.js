67b302560cb0d29dcba6c6a6aee4dfd531a258ad
Daniel Amoateng <Danielamoateng81@yahoo.com>
2021-04-19 18:47:06 -0400

    Sets up a function that finds and returns each element and it's count

---
+++ index.js
@@ -0,0 +1,14 @@
+
+//Finds and returns each element and it's count in the array with a new object
+const compressedArray = (original) => {
+  let compressed = []
+}
+
+
+// eslint-disable-next-line no-unused-vars
+const validateLineup = (lineup) => {
+  return true
+}
+
+
+module.exports = validateLineup
