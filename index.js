bfb32ae6cefdb60530c402cd9d1d53e6cd2a8f39
Daniel Amoateng <Danielamoateng81@yahoo.com>
2021-04-19 18:52:45 -0400

    Declare copy variable to make copy of input array

--- index.js
+++ index.js
@@ -2,6 +2,38 @@
 //Finds and returns each element and it's count in the array with a new object
 const compressedArray = (original) => {
   let compressed = []
+
+
+  //Make a copy of the input array
+  let copy = original.slice(0)
+
+
+  //Loop over every element in the 
+  for(let i=0; i < original.length; i++)
+   let myCount = 0
+
+
+   //looping over every element in the copy to see 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
